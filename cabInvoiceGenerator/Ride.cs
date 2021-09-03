b04286aa440f146509932ab33ab2a4e8a119d1c6
kandula-prakash <abhinayprakash143@gmail.com>
2021-09-03 21:45:46 +0530

    feat: canInvoiceGenarator

---
+++ cabInvoiceGenerator/Ride.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cabInvoiceGenerator
+{
+  public  class Ride
+    {
+        public class Ride
+        {
+            public double distance;
+            public int time;
+            public Ride(double distance, int time)
+            {
+                this.distance = distance;
+                this.time = time;
+
+            }
+        }
+    }
+}
---
+++ cabInvoiceGenerator/RideType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cabInvoiceGenerator
+{
+   public class RideType
+    {
+        public enum RideType
+        {
+            NORMAL,
+            PREMIUM
+
+        }
+    }
+}
